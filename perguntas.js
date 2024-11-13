38bdb0caa073d38d231175c2130fc61ffa37b0aa
gargantilha <shss15br@gmail.com>
2024-11-13 11:10:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mercenários 2',
+    'Quem é o líder dos Mercenários no filme "Mercenários 2"?',
+    'O líder dos Mercenários é Barney Ross, interpretado por Sylvester Stallone'
+)
+
+criaCartao(
+    'Mercenários 2',
+    'Qual ator interpreta o personagem "Church" em "Mercenários 2"?',
+    'O personagem "Church" é interpretado por Bruce Willis'
+)
+
+criaCartao(
+    'Mercenários 2',
+    'Qual é o vilão principal de "Mercenários 2"?',
+    'O vilão principal de "Mercenários 2" é Vilain, interpretado por Jean-Claude Van Damme'
+)
+
+criaCartao(
+    'Mercenários 2',
+    'Quem são os Mercenários no filme "Mercenários 2"?',
+    'Os Mercenários são um grupo de mercenários experientes e habilidosos, incluindo personagens interpretados por Sylvester Stallone, Jason Statham, Jet Li, Dolph Lundgren, Chuck Norris, Bruce Willis e Arnold Schwarzenegger'
+)
